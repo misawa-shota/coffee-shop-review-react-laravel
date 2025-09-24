7dee37f563f414a3c89fe3635d02f985974cf14b
misawa-shota <kaiyou0120@gmail.com>
2025-09-24 16:08:22 +0900

    ホーム画面の作成

---
+++ my-app/app/Http/Controllers/ShopController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Shop;
+use App\Models\Review;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class ShopController extends Controller
+{
+    public function index()
+    {
+        // $shops = Shop::all();
+        $shops = Shop::with('reviews')->get();
+        // dd($shops);
+        // 新着のレビューを5件取得
+        $newReviews = Review::With('shop', 'user')
+            ->orderBy('created_at', 'desc')
+            ->take(5)
+            ->get();
+
+        return Inertia::render('Home', [
+            'shops' => $shops,
+            'newReviews' => $newReviews,
+        ]);
+    }
+}
